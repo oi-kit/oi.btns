f25ce8feea858772f2d7bae3760e85bb247729a5
Esutarosa <esutarosa.git@gmail.com>
2024-09-21 03:32:30 +0300

    style: edit tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,24 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "3xl": "1140px",
+        },
+      },
+      extends: {
+        borderRadius: {
+          lg: "1rem",
+          md: "0.75rem",
+          sm: "0.5rem",
+        },
       },
     },
   },
